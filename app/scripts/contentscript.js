c041a75b2b27746e181e12fc57499ce8765f3ac8
John Armstrong <j.armstrong484@gmail.com>
2017-06-11 11:36:12 +1200

    Add source file for content script

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CUSTOM_BUTTON = '<a class="button-link">Create issue</a>';
+var CUSTOM_BUTTON = '<a class="button-link trello-github">Create issue</a>';
 
 $(document).ready(function () {
   console.log('Running');
@@ -9,13 +9,30 @@ $(document).ready(function () {
     //Only add our custom button if the list we want to add it to has been added to page
     if (listAdded(event.target)) {
 
-      const list = $('div.window-sidebar > div:nth-child(1) > div');
+      var list = $('div.window-sidebar > div:nth-child(1) > div');
       $(list).append(CUSTOM_BUTTON);
+
+      $(list).find('.trello-github').on('click', onClick);
     }
   });
 
-  const listAdded = (element) => {
+  var listAdded = function listAdded(element) {
     //In Trello the list we want to add to has the title "Add" so look for that
-    return $(element).find('div.window-module > h3:first').text() == "Add";
-  }
+    return $(element).find('div.window-module > h3:first').text() == 'Add';
+  };
+
+  var onClick = function onClick(event) {
+    var popOver = $('.pop-over');
+
+    $(popOver).append(popUp);
+    $(popOver).addClass('is-shown');
+    $(popOver).css({ 'top': event.pageY, 'left': event.pageX });
+
+    $('.pop-over-header-close-btn').on('click', function () {
+      $(popOver).removeClass('is-shown');
+      $(popOver).html('');
+    });
+  };
 });
+
+var popUp = '\n  <div class="content">\n    <div class="pop-over-header">\n      <span class="pop-over-header-title">Create issue in repo:</span>\n      <a href="#" class="pop-over-header-close-btn icon-sm icon-close"></a>\n    </div>\n    <div class="pop-over-content">\n      <ul class="pop-over-list">\n        <li><a href="#">Repo 1</a></li>\n        <li><a href="#"> Repo 2</a></li>\n      </ul>\n    </div>\n  </div>\n';

362a1c421a0588a5e5f44e1f5633b480273ca733
John Armstrong <j.armstrong484@gmail.com>
2017-06-10 16:07:46 +1200

    Add our custom button when the list we want to add it to is loaded

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,21 @@
 'use strict';
 
-$(document).ready(() => { });
+const CUSTOM_BUTTON = '<a class="button-link">Create issue</a>';
+
+$(document).ready(function () {
+  console.log('Running');
+  $('body').on('DOMNodeInserted', function (event) {
+
+    //Only add our custom button if the list we want to add it to has been added to page
+    if (listAdded(event.target)) {
+
+      const list = $('div.window-sidebar > div:nth-child(1) > div');
+      $(list).append(CUSTOM_BUTTON);
+    }
+  });
+
+  const listAdded = (element) => {
+    //In Trello the list we want to add to has the title "Add" so look for that
+    return $(element).find('div.window-module > h3:first').text() == "Add";
+  }
+});
